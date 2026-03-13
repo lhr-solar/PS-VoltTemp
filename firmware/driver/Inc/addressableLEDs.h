8788d5a54725d34d8e9020572dbff50ae027f9f6
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2026-03-13 15:52:19 -0500

    completely untested on hw :3

---
+++ firmware/driver/Inc/addressableLEDs.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "common.h"
+#include "stm32xx_hal.h"
+#include "WS2812B.h"
+
+#define FAULT_COLOR WS2812B_SOLID_RED
+#define NO_FAULT_COLOR WS2812B_SOLID_GREEN
+
+typedef enum{
+    MODULE_0_TEMP_LED = 0,
+    MODULE_1_TEMP_LED,
+    MODULE_2_TEMP_LED,
+    MODULE_3_TEMP_LED,
+    MODULE_0_VOLTAGE_LED,
+    MODULE_1_VOLTAGE_LED,
+    MODULE_2_VOLTAGE_LED,
+    MODULE_3_VOLTAGE_LED,
+    MODULE_4_VOLTAGE_LED
+}addressable_led_idx;
+
+
+ws2812b_status_t addressableLEDInit();
+
+ws2812b_status_t clearAddressableColors(TickType_t delay_ticks);
+
+ws2812b_status_t setModuleFaultLed(addressable_led_idx moduleLED, ws2812b_color_t color, TickType_t delay_ticks);
---
+++ firmware/tests/addressableLEDs_test.c
@@ -0,0 +1,52 @@
+#include "stm32xx_hal.h"
+#include "addressableLEDs.h"
+#include "leds.h"
+#include "WS2812B.h"
+
+StaticTask_t TaskBuffer;
+StackType_t TaskStack[configMINIMAL_STACK_SIZE];
+
+void task(){
+
+    clearAddressableColors(portMAX_DELAY);
+
+    setModuleFaultLed(MODULE_1_TEMP_LED, FAULT_COLOR, portMAX_DELAY);
+    while(1){
+
+        toggle_heartbeat();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+int main(){
+
+    HAL_Init();
+    SystemClock_Config();
+
+    leds_init();
+
+    ws2812b_status_t status = addressableLEDInit();
+    if(status == WS2812B_OK){
+        set_heartbeat_led(ON);
+    }
+
+
+    // Create the tasks statically
+    xTaskCreateStatic(task, 
+                     "TX",
+                     configMINIMAL_STACK_SIZE,
+                     NULL,
+                     tskIDLE_PRIORITY + 2,
+                     TaskStack,
+                     &TaskBuffer);
+
+    // Start the scheduler
+    vTaskStartScheduler();
+
+    while(1){
+        // should never get here
+    }
+
+    return 0;
+
+}
